c896449873b0c5b8a6e2f611db3fc23e076857b3
Travis <builds@travis-ci.com>
2020-01-29 16:53:37 +0000

    Use CI_SERVER_HOST to determine the host name.

--- R/gitlab_instead_of.R
+++ R/gitlab_instead_of.R
@@ -2,14 +2,15 @@
 #' @title Gitlab rewrite
 #'
 #' Rewrites the gitlab URL. This can be handy if you want to push to another GitLab repo
-#' on successful builds.
+#' on successful builds. Uses the environment variable CI_SERVER_HOST to determine
+#' the HOST.
 #' @export
 gitlab_instead_of = function() {
+  SERVER_HOST = Sys.getenv("CI_SERVER_HOST")
   system2("git",
           args = c("config",
-                   '--global url."https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.com/".insteadOf',
-                   '"git@gitlab.com:"'))
+                   '--global url."https://gitlab-ci-token:${CI_JOB_TOKEN}@{SERVER_HOST}/".insteadOf',
+                   '"git@{SERVER_HOST}:"'))
   return(invisible(NULL))
 }
-
 # nolint end
