8933c0e0219c0323793302e7db77eb866339a07a
Colin Gillespie <csgillespie@gmail.com>
2020-02-22 13:26:37 +0000

    Only check files that have changed (#10)

--- R/git_hooks.R
+++ R/git_hooks.R
@@ -1,3 +1,41 @@
+#' @export
+#' @rdname add_pre_commit
+#' @description git_pre_commit only tests files that have changed. Makes things, especially linting
+#' a lot faster.
+git_pre_commit = function() {
+  out = system2("git",  args = c("diff", "--name-only", "--cached"), stdout = TRUE)
+  fnames = unlist(stringr::str_split(out, "\n"))
+
+  if (any(stringr::str_detect(fnames, "DESCRIPTION"))) {
+    check_tidy_description()
+  }
+
+  if (any(stringr::str_starts(fnames, "R\\/"))) {
+    check_r_filenames()
+  }
+
+  if (any(stringr::str_detect(fnames, "NAMESPACE"))) {
+    check_namespace()
+  }
+
+  has_error = FALSE
+  fnames = fnames[stringr::str_detect(fnames, "\\.R$|\\.r$|\\.Rmd$")]
+  for (fname in fnames) {
+    msg_info(paste("Checking ", fname))
+    l = lintr::lint(fname)
+    if (length(l) > 0) {
+      print(l)
+      has_error = TRUE
+    }
+  }
+  if (isTRUE(has_error)) {
+    msg_error("Linting errors", stop = TRUE)
+  } else {
+    msg_ok("Linting OK")
+  }
+  return(invisible(NULL))
+}
+
 add_hook = function(type, fname) {
   fname = file.path(system.file("templates", package = "inteRgrate"), fname)
   if (!file.exists(fname)) {
