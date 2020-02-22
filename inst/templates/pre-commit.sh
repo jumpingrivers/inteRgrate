60f9989b6beb51c230cdac0464800735a7121505
csgillespie <builds@travis-ci.com>
2020-02-22 11:21:09 +0000

    Only check files that have changed

--- inst/templates/pre-commit.sh
+++ inst/templates/pre-commit.sh
@@ -1,32 +1,6 @@
 #!/bin/bash
 
-Rscript -e "inteRgrate::check_r_filenames()"
+Rscript -e "inteRgrate::git_pre_commit()"
 if [ "$?" -eq 1 ]; then
   exit 1
 fi
-
-Rscript -e "inteRgrate::check_lintr()"
-if [ "$?" -eq 1 ]; then
-  exit 1
-fi
-
-# This actually changes the description file.
-Rscript -e "inteRgrate::check_tidy_description()"
-if [ "$?" -eq 1 ]; then
-  exit 1
-fi
-
-Rscript -e "inteRgrate::check_namespace()"
-if [ "$?" -eq 1 ]; then
-  exit 1
-fi
-
-# Running check_version doesn't currently work.
-# Basically I need to figure out what is being compared
-# If commit 1 update version
-# Commit 2 doesn't need to update
-# So need to comparse all commits to some "branch/master"
-# What happens if branch doesn't exist on origin ....
-
-# It would also be nice to scan gitlab-ci /travis and extract the right hooks
-
