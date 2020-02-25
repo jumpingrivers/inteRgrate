948d1b49287e031d287636480d3e91cf28e6b8b7
Maria Garcia <maria@example.com>
2020-02-25 15:32:39 +0000

    Travis

--- tmp.R
+++ tmp.R
@@ -1,4 +1,5 @@
-ssh_id = Sys.getenv("id_rsa", "")
+ssh_id = Sys.getenv("id_rsa", NA)
+if (is.na(ssh_id)) stop("NO RSA")
 ssh_id_file <- "~/.ssh/id_rsa"
-write_lines(rawToChar(openssl::base64_decode(ssh_id)), ssh_id_file)
+readr::write_lines(rawToChar(openssl::base64_decode(ssh_id)), ssh_id_file)
 fs::file_chmod(ssh_id_file, "0600")
