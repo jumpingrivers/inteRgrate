dbedcbeab5aaed6c3c8b225a3e986b92dacd8342
Colin Gillespie <csgillespie@gmail.com>
2020-02-26 14:34:26 +0000

    Travis

--- R/create_tag.R
+++ R/create_tag.R
@@ -24,14 +24,14 @@ globalVariables(c("tag_name", "SERVER_HOST", "CI_PROJECT_ID", "CI_COMMIT_SHA"))
 #' @export
 create_tag = function(branch = "master", in_development = FALSE) {
   set_crayon()
+
   msg_start("Creating a tag...create_tag()")
   if (!is_gitlab() || !is_github()) {
     msg_info("Doesn't seem to be a gitlab runner, so no tagging")
     return(invisible(NULL))
   }
 
-  if (!is.na(Sys.getenv("CI_COMMIT_TAG", NA)) &&
-      !is.na(Sys.getenv("TRAVIS_TAG", NA))) {
+  if (is_tagging_branch()) {
     msg_info("This looks like a tagging CI process, so no tagging")
     return(invisible(NULL))
   }
@@ -41,14 +41,11 @@ create_tag = function(branch = "master", in_development = FALSE) {
     return(invisible(NULL))
   }
 
-  # Assume GITLAB
-  if (Sys.getenv("CI_COMMIT_BRANCH") != branch) {
+  # Check branch
+  if (get_current_branch() != branch) {
     msg_info(paste("Not on", branch, "so no tagging"))
     return(invisible(NULL))
   }
-
-
-
 
   if (isFALSE(has_pkg_changed(repo = paste0("origin/", branch)))) {
     msg_info("Package hasn't changed, so no tagging")
@@ -56,6 +53,32 @@ create_tag = function(branch = "master", in_development = FALSE) {
   }
 
   tag_name = get_tag_name()
+  if (is_gitlab()) {
+    gitlab_tag(tag_name)
+  } else {
+    github_tag(tag_name)
+  }
+  msg_ok("Tagging good.")
+  return(invisible(NULL))
+}
+
+github_tag = function(tag_name) {
+  system2("git", args = c("config", "--global", "user.email",  "'travis.tagger@example.com'"))
+  system2("git", args = c("config", "--global", "user.name", "'Travis tagger'"))
+  github_pat = Sys.getenv("GITHUB_PAT", NA)
+  if (is.na(github_pat)) {
+    msg_error("GITHUB_PAT missing. Required for tagging", stop = TRUE)
+  }
+  repo = Sys.getenv("TRAVIS_REPO_SLUG", NA)
+  git_url = glue::glue("https://{github_pat}@github.com/{repo}.git")
+  system2("git", args = c("remote", "set-url", "origin", git_url))
+  system2("git", args = c("tag", "-a", tag_name,  "-m", glue::glue("Version {tag_name}")))
+  system2("git", args = c("push", "--tags"))
+  return(invisible(NULL))
+
+}
+
+gitlab_tag = function(tag_name) {
   gitlab_instead_of()
   SERVER_HOST = Sys.getenv("CI_SERVER_HOST") #nolint
   CI_PROJECT_ID = Sys.getenv("CI_PROJECT_ID") #nolint
