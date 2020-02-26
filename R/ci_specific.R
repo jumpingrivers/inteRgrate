ec43219977564c7fd44e7738c595c1a30da10471
Colin Gillespie <csgillespie@gmail.com>
2020-02-26 16:44:54 +0000

    Moving towards a release

--- R/ci_specific.R
+++ R/ci_specific.R
@@ -8,6 +8,23 @@ is_tagging_branch = function() {
     nchar(Sys.getenv("TRAVIS_TAG")) > 0L
 }
 
+get_auth_token = function() {
+  if (is_gitlab()) {
+    token = Sys.getenv("GITLAB_TOKEN", NA) #nolint
+    if (is.na(token)) {
+      msg_error("GITLAB_TOKEN missing. Required for tagging", stop = TRUE)
+    }
+  }
+
+  if (is_github()) {
+    token = Sys.getenv("GITHUB_PAT", NA)
+    if (is.na(token)) {
+      msg_error("GITHUB_PAT missing. Required for tagging", stop = TRUE)
+    }
+  }
+  return(token)
+}
+
 
 #' @importFrom stringr str_detect str_trim str_split
 get_env_variable = function(env_variable, default = NULL) {
