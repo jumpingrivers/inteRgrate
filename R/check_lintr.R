4f0b927d7aacf087c7b470a571d12e96f4832101
Colin Gillespie <csgillespie@gmail.com>
2020-06-02 14:58:18 +0100

    Lint all files

--- R/check_lintr.R
+++ R/check_lintr.R
@@ -1,13 +1,13 @@
 #' @title The lintr check
 #'
-#' Runs lint from the \code{lintr} package.
-#' @param readme Default \code{TRUE}. Should README.Rmd be checked
-#' @param vignettes Default \code{TRUE}. Should the vignettes be linted.
+#' Runs lint from the \code{lintr} package. Also scans for all
+#' \code{.Rmd} and \code{.R} files in other directories.
 #' @importFrom lintr lint_package lint_dir lint
 #' @export
-check_lintr = function(readme = TRUE, vignettes = TRUE) {
+check_lintr = function() {
   set_crayon()
   cli::cli_h3("Checking lint...check_lintr()")
+  lint_errors = FALSE
 
   if (!file.exists(".lintr")) {
     cli::cli_alert_info("No .lintr file found")
@@ -16,29 +16,23 @@ check_lintr = function(readme = TRUE, vignettes = TRUE) {
   lints = lintr::lint_package()
   if (length(lints) > 0) {
     lapply(lints, print)
-    msg_error("Please fix lints in R/", stop = TRUE)
-  }
-  cli::cli_alert_success("R/ scripts OK")
-
-  if (isTRUE(readme) && file.exists("README.Rmd")) {
-    cli::cli_h3("Checking README.Rmd")
-    re_lints = lintr::lint("README.Rmd")
-    if (length(re_lints) > 0) {
-      lapply(re_lints, print)
-      msg_error("Please fix lints in the README.Rmd", stop = TRUE)
-    }
-    cli::cli_alert_success("README.Rmd OK")
+    lint_errors = TRUE
   }
 
-  if (isTRUE(vignettes) && file.exists("vignettes")) {
-    cli::cli_h3("Checking vignettes")
-    vig_lints = lintr::lint_dir("vignettes", pattern = "*.Rmd")
-    if (length(vig_lints) > 0) {
-      lapply(vig_lints, print)
-      msg_error("Please fix lints in vignettes/", stop = TRUE)
+  fnames = list.files(path = ".", pattern = "\\.(Rmd|R|r)$", recursive = TRUE)
+  fnames = fnames[stringr::str_detect(fnames, pattern = "^R/", negate = TRUE)]
+  for (fname in fnames) {
+    r_lint = lintr::lint(fname)
+    if (length(r_lint) > 0) {
+      lapply(r_lint, print)
+      lint_errors = TRUE
     }
-    cli::cli_alert_success("Vignettes OK")
   }
+
+  if (isTRUE(lint_errors)) {
+    msg_error("Please fix linting errors", stop = TRUE)
+  }
+
   cli::cli_alert_success("Lint looks good")
   return(invisible(NULL))
 }
