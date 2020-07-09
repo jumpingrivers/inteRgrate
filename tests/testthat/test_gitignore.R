905e2bf4bb480f53e498f9aa886cc91175c71898
Colin Gillespie <csgillespie@gmail.com>
2020-07-09 15:17:26 +0100

    Remove test

--- tests/testthat/test_gitignore.R
+++ tests/testthat/test_gitignore.R
@@ -1,6 +1,5 @@
 test_that("Testing gitignore", {
 
-  expect_error(check_gitignore(path = system.file("tests/testthat", package = "inteRgrate")))
   good_pkg = system.file("good_pkg", package = "inteRgrate")
   expect_null(check_gitignore(path = good_pkg))
 })
