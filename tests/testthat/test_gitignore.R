52d6699ca41d7fd6631793f558d7981fedf3e141
Colin Gillespie <csgillespie@gmail.com>
2020-07-09 14:53:51 +0100

    Don't use relative paths in tests

--- tests/testthat/test_gitignore.R
+++ tests/testthat/test_gitignore.R
@@ -1,6 +1,6 @@
 test_that("Testing gitignore", {
-  good_pkg = system.file("good_pkg", package = "inteRgrate")
 
-  expect_error(check_gitignore(path = "."))
+  expect_error(check_gitignore(path = system.file("tests/testthat", package = "inteRgrate")))
+  good_pkg = system.file("good_pkg", package = "inteRgrate")
   expect_null(check_gitignore(path = good_pkg))
 })
