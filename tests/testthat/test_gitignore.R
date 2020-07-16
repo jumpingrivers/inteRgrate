d3a1aab6b34a2e669cb3bd7b3a7795a071bc6e47
Colin Gillespie <csgillespie@gmail.com>
2020-07-16 19:56:24 +0100

    Update test

--- tests/testthat/test_gitignore.R
+++ tests/testthat/test_gitignore.R
@@ -1,5 +1,8 @@
 test_that("Testing gitignore", {
 
   good_pkg = system.file("good_pkg", package = "inteRgrate")
-  expect_null(check_gitignore(path = good_pkg))
+  gitignore = file.path(good_pkg, "gitignore")
+  file.copy(gitignore, ".gitignore", overwrite = TRUE)
+  on.exit(file.remove(".gitignore"))
+  expect_null(check_gitignore(path = "."))
 })
