2be200a131ed042346f16763ac1b2d6d4b3c6709
Russ Hyde <russ.hyde.data@gmail.com>
2022-02-22 12:15:19 +0000

    copy DESCRIPTIONs to temp dir before assessing structure

--- tests/testthat/test_tidy_description.R
+++ tests/testthat/test_tidy_description.R
@@ -3,12 +3,17 @@ test_that("Testing tidy description", {
   ## Hard to test, as use_tidy_description looks at the current .Rproj
   ## XXX: Could create a dummy .Rproj file?
 
-  description = file.path(system.file("test_news", package = "inteRgrate"), "DESCRIPTION-major")
-  file.copy(description, to = "DESCRIPTION", overwrite = TRUE)
-  expect_null(check_tidy_description())
-  description = file.path(system.file("test_news", package = "inteRgrate"), "DESCRIPTION-dev")
-  file.copy(description, to = "DESCRIPTION", overwrite = TRUE)
-  expect_null(check_tidy_description())
+  withr::with_dir(
+    tempdir(), {
+      description = file.path(system.file("test_news", package = "inteRgrate"), "DESCRIPTION-major")
+      file.copy(description, to = "DESCRIPTION", overwrite = TRUE)
+      expect_null(check_tidy_description())
+  })
 
-
+  withr::with_dir(
+    tempdir(), {
+      description = file.path(system.file("test_news", package = "inteRgrate"), "DESCRIPTION-dev")
+      file.copy(description, to = "DESCRIPTION", overwrite = TRUE)
+      expect_null(check_tidy_description())
+  })
 })
